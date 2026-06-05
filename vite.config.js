cb2bbe3b71cf47a3102a6d3339f6da1a19fd0515
ThaiMecc19 <sumstt@gmail.com>
2026-06-05 12:14:22 +0700

    UI Fixed

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,24 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { fileURLToPath } from 'node:url'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+
   resolve: {
-    alias: [
-      {
-        find: /^moment$/,
-        replacement: fileURLToPath(new URL('./node_modules/moment/moment.js', import.meta.url)),
-      },
-    ],
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+
+      // moment fix (ถ้าจำเป็นจริง)
+      moment: fileURLToPath(
+        new URL("./node_modules/moment/moment.js", import.meta.url)
+      ),
+    },
   },
+
   optimizeDeps: {
-    include: ['calendarjs', 'moment'],
+    include: ["calendarjs", "moment"],
   },
 })
